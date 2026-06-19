3bd6583de1ad0f4e5a7fe45f4b698e97dd909b76
mollen <stepanpanenko@gmail.com>
2026-06-19 20:24:57 +0300

    MorseCode entity

---
+++ src/main/java/org/mollen/entity/MorseCode.java
@@ -0,0 +1,31 @@
+package org.mollen.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "morse_codes")
+public class MorseCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uuid")
+    @JdbcTypeCode(SqlTypes.UUID)
+    private UUID uuid;
+
+    @Column(name = "task_text")
+    private String taskText;
+
+    @Column(name = "morse_text")
+    private String morseText;
+
+    @Column(name = "question")
+    private String question;
+}
